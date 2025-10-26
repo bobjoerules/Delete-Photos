1b86187444050d97cd69bfdc90866bc245d478e7
Bobjoerules <yo.bob.joe.rules@gmail.com>
2025-10-26 15:57:48 -0700

    Added share button and image count. Also reworded discription in settings for reset button

---
+++ Delete Photos Tests/Delete_Photos_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Delete_Photos_Tests.swift
+//  Delete Photos Tests
+//
+//  Created by Bobjoerules on 10/26/25.
+//
+
+import Testing
+
+struct Delete_Photos_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
